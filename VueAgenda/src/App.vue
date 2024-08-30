cb005589db57627e03a9efd094e22abb3e72b0bc
josehb96 <josehurtadobaeza@gmail.com>
2024-08-30 07:20:14 +0200

    Vue Component & TailwindCSS Installation and Config

--- VueAgenda/src/App.vue
+++ VueAgenda/src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
-
+  const title = 'Mi Agenda - Lista de contactos'
+  
+  const buscarContacto = () => {
+    console.log('buscando contacto ...') 
+  }
 </script>
 
 <template>
   <div>
-
+    <h1>{{ title }}</h1>
+    <div>
+      <input type="text">
+      <button @click="buscarContacto">Buscar</button>
+    </div>
   </div>
 </template>
 
