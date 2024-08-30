4e17bdd856a3bb8fc78e2bd2d0e5b028eda9ec5f
josehb96 <josehurtadobaeza@gmail.com>
2024-08-30 18:43:27 +0200

    Diseño de pantalla con TailwindCSS

--- VueAgenda/src/App.vue
+++ VueAgenda/src/App.vue
@@ -7,11 +7,29 @@
 </script>
 
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="flex items-center flex-col p-5">
+    <h1 class="text-xl m-4">{{ title }}</h1>
     <div>
       <input type="text">
       <button @click="buscarContacto">Buscar</button>
+    </div>
+    <div>
+      <table>
+        <thead>
+          <th>Foto</th>   
+          <th>Nombres</th>   
+          <th>numero</th>   
+          <th>correo</th>   
+        </thead>   
+        <tbody>
+          <tr>
+            <td></td>  
+            <td>Monkey Luffy</td>
+            <td>00124578</td>
+            <td>reypirata@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>   
     </div>
   </div>
 </template>
