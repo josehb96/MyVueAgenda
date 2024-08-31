cb87e78dd7e41dbbfaeb6b5bb83ef87d0b584297
josehb96 <josehurtadobaeza@gmail.com>
2024-08-31 05:59:12 +0200

    Which is a state?

--- VueAgenda/src/App.vue
+++ VueAgenda/src/App.vue
@@ -1,20 +1,31 @@
 <script setup>
+  import { ref } from 'vue';
   import SearchContacts from './components/SearchContacts.vue'; 
   import ListContacts from './components/ListContacts.vue';
   
   // 
   import { getAllContacts } from './services/apiContacts';
-  
+import { list } from 'postcss';
+
+  //
+  // let listContacts = getAllContacts()
+  const listContacts = ref(getAllContacts()) // Ahora esta variable se ha convertido en un estado de Vue al usar la directiva ref
   const title = 'Mi Agenda - Lista de contactos'
 
-  const listContacts = getAllContacts() 
+  const clearList = () => {
+    listContacts.value = []
+  }  
 
 </script>
 
 <template>
   <div class="flex items-center flex-col p-5">
     <h1 class="text-xl m-4">{{ title }}</h1>
-    <SearchContacts />
+    <SearchContacts listContacsts="listContacts"/>
     <ListContacts title="Lista de Resultados" :listContacts="listContacts"/>     
+
+    <div>
+      <button class="bg-blue-500 py-1.5 px-5" @click="clearList">Limpiar</button> 
+    </div>
   </div>
 </template>
